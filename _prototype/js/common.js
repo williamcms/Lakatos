5acf64e808b3432393bc8f158a6b8ff39b92eada
Williamcms <williamcms_@outlook.com>
2022-01-17 21:22:32 -0300

    Added a few more sections to prototype

--- _prototype/js/common.js
+++ _prototype/js/common.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+	//Slickers
+	$('.slick-carousel-dots').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: false,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+		nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slick-carousel-arrows').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+		nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slick-carousel-both').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+		nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 	// Menu Hover
 	$("header nav.menu ul li").hover(
 		function(){
