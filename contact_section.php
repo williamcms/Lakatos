2f7969a1781427e4d77ee7667953a2d59cdb7afe
Williamcms <williamcms_@outlook.com>
2022-07-12 18:41:13 -0300

    Moção da seção "Formulário de contato" para um arquivo separado, visto que será utilizada em pelo menos mais uma página

---
+++ contact_section.php
@@ -0,0 +1,32 @@
+<div class="section contactus" id="contato">
+	<h2 class="center">Formulário de contato</h2>
+	<div class="section-separator-1"></div>
+	<form method="POST">
+		<div class="form-group row">
+			<div class="input-group col">
+				<div class="input-group-prepend"><label class="input-group-text" for="contatonome">Nome:</label></div>
+				<input type="text" name="nome" id="contatonome" />
+			</div>
+			<div class="input-group col">
+				<div class="input-group-prepend"><label class="input-group-text" for="contatotelefone">Telefone:</label></div>
+				<input type="tel" name="telefone" id="contatotelefone" />
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="input-group col">
+				<div class="input-group-prepend"><label class="input-group-text" for="contatoemail">Email:</label></div>
+				<input type="email" name="email" id="contatoemail" />
+			</div>
+			<div class="input-group col">
+				<div class="input-group-prepend"><label class="input-group-text" for="contatoassunto">Assunto:</label></div>
+				<input type="text" name="assunto" id="contatoassunto" />
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="input-group">
+				<textarea name="mensagem" id="contatomensagem" placeholder="Mensagem"></textarea>
+			</div>
+		</div>
+		<button class="button2 btn-brand"><span>Enviar</span></button>
+	</form>
+</div>
