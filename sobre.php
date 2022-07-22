cfe801fdb598b31f0fd3bae1df5b51904ccf843c
Williamcms <williamcms_@outlook.com>
2022-07-22 19:58:17 -0300

    Remoção da pasta _prototype e moção de alguns arquivos dela para áreas mais adequadas e dos outros itens de referência para a pasta _ref

---
+++ sobre.php
@@ -0,0 +1,30 @@
+<?php require_once('cfg/core.php'); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<title><?php echo $website->title; ?></title>
+	<!-- Chart API -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" defer></script>
+	<?php require_once('header-front.php'); ?>
+</head>
+<body>
+	<?php require_once('menu.php'); ?>
+
+	<main class="aboutus">
+		<div class="row">
+			<div class="col">
+				<h1>Sobre nós</h1>
+				<p>Referência no mercado de manufatura de máquinas termoformadoras há mais de 45 anos, a Eletro-Forming passou a se chamar Lakatos. A mudança, ocorrida no início de 2019, aconteceu como parte de uma estratégia de reposicionamento da marca no mercado. O antigo nome deu lugar ao sobrenome Lakatos. </p><br/>
+				<p>Sediada no município de Embu das Artes (SP), a Lakatos conta com uma área de produção de 2500 m². O espaço conta com departamento de usinagem, caldeiraria, serralheria, montagem, pintura, metrologia e, também, um estoque de peças para pronto-atendimento.</p><br/>
+				<p>A empresa conta com uma equipe altamente habilitada para desenvolvimento de projetos e incorporação das mais modernas tecnologias. Sua linha de produção engloba máquinas de usinagem CNC capazes de manufaturar variadas peças e moldes complexos. </p><br/>
+				<p>Os departamentos de Engenharia e Produção da Lakatos são equipados com softwares (CAE, CAD 3D, EPLAN e CAM) de última geração. Um sistema ERP integra as diversas áreas da companhia.</p>
+			</div>
+			<div class="col-auto">
+				<img src="_prototype/images/lakatos_empresa.jpg" alt="Foto Aérea" width="800" height="540" />
+			</div>
+		</div>
+	</main>
+
+	<?php require_once('footer.php'); ?>
+</body>
+</html>
