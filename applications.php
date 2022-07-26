c70e6cbd373cc99665292a547bfdaa3c2fd51fa5
Williamcms <williamcms_@outlook.com>
2022-07-26 18:02:46 -0300

    Alteração dos itens que são exibidos em aplicações, itens anteriores foram movidos para a página de embalagens e adição de link para item específico na Home/Index

--- applications.php
+++ applications.php
@@ -5,7 +5,7 @@
 		$pagename = explode('.', $_GET['pagename'])[0];
 
 		$conn->link = $conn->connect();
-		if($stmt = $conn->link->prepare("SELECT * FROM packaging WHERE pack_name = ? LIMIT 1")){
+		if($stmt = $conn->link->prepare("SELECT * FROM applications WHERE ap_name = ? LIMIT 1")){
 			try{
 				$stmt->bind_param('s', $pagename);
 				$stmt->execute();
@@ -27,48 +27,37 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-	<title><?php echo $website->title .': '. (isset($pagename) ? $row[0]['pack_name'] : 'Embalagens') ?></title>
+	<title><?php echo $website->title .': '. (isset($pagename) && $stmt->num_rows ? $row[0]['ap_name'] : 'Embalagens') ?></title>
 	<?php require_once('header-front.php'); ?>
 </head>
 <body>
 	<?php require_once('menu.php'); ?>
 
-	<main>
+	<main class="applications">
 		<?php
 			if(isset($_GET['pagename']) && $stmt->num_rows){
-				$pack_id = $row[0]['pack_id'];
-				if($stmt2 = $conn->link->prepare("SELECT * FROM application_packaging WHERE appack_pack = ? ORDER BY appack_id ASC")){
+				$ap_id = $row[0]['ap_id'];
+				if($stmt2 = $conn->link->prepare("SELECT * FROM application_packaging WHERE appack_ap = ? ORDER BY appack_id ASC")){
 					try{
-						$stmt2->bind_param('i', $pack_id);
+						$stmt2->bind_param('i', $ap_id);
 						$stmt2->execute();
-						$pack_ids = get_result($stmt2);
+						$appack_ids = get_result($stmt2);
 					}
 					catch(Exception $e){
 						throw new Exception('Erro ao conectar com a base de dados: '. $e);
 					}
 				}
 
-				if($stmt3 = $conn->link->prepare("SELECT * FROM machine_packaging WHERE mp_pack = ?")){
+				$packaging_ids_query = '';
+				for($i = 0; $i < $stmt2->num_rows; $i++) {
+					$packaging_ids_query .= ($i == 0 ? 'SELECT * FROM packaging WHERE pack_active = 1 AND (pack_id  = '.$appack_ids[$i]['appack_pack'] : ' OR pack_id  = '. $appack_ids[$i]['appack_pack']);
+				}
+				$packaging_ids_query .= ')';
+
+				if($stmt3 = $conn->link->prepare($packaging_ids_query)){
 					try{
-						$stmt3->bind_param('i', $pack_id);
 						$stmt3->execute();
-						$machinesToPackaging = get_result($stmt3);
-					}
-					catch(Exception $e){
-						throw new Exception('Erro ao conectar com a base de dados: '. $e);
-					}
-				}
-
-				$machines_ids_query = '';
-				for($i = 0; $i < $stmt3->num_rows; $i++) {
-					$machines_ids_query .= ($i == 0 ? 'SELECT * FROM machines WHERE mac_active = 1 AND (mac_id = '.$machinesToPackaging[$i]['mp_mac'] : ' OR mac_id = '. $machinesToPackaging[$i]['mp_mac']);
-				}
-				$machines_ids_query .= ')';
-
-				if($stmt4 = $conn->link->prepare($machines_ids_query)){
-					try{
-						$stmt4->execute();
-						$machines_result = get_result($stmt4);
+						$packaging_result = get_result($stmt3);
 					}
 					catch(Exception $e){
 						throw new Exception('Erro ao conectar com a base de dados: '. $e);
@@ -76,41 +65,34 @@
 				}
 				echo '<article class="product">
 						<section class="image">
-							'. (!$browser->isMobile() ? '<picture style="background: url('. $row[0]['pack_image'] .') no-repeat;"></picture>' : 
-								'<picture><img src="'. $row[0]['pack_image'] .'" alt="'. $row[0]['pack_name'] .'" /></picture>') .'
+							'. (!$browser->isMobile() ? '<picture style="background: url('. $row[0]['ap_banner'] .') no-repeat;"></picture>' : 
+								'<picture><img src="'. $row[0]['ap_banner'] .'" alt="'. $row[0]['pack_name'] .'" /></picture>') .'
 						</section>
 
 						<section class="info">
 							<div class="title pt-3 pl-3 pr-4">
-								<h1>'. $row[0]['pack_name'] .'</h1>
+								<h1>'. $row[0]['ap_name'] .'</h1>
 							</div>
 
 							<div class="description row">
 								<div class="col">
-									'. $row[0]['pack_desc'] .'
+									'. $row[0]['ap_desc'] .'
 								</div>
-
-								<div class="col-auto">';
-									echo '<div class="d-flex"><span class="bold">Dimensões</span>:&nbsp;<div class="text">'. $row[0]['pack_sizes'] .'</div></div>';
-									echo '<div class="d-flex"><span class="bold">Cavidades</span>:&nbsp;<div class="text">'. $row[0]['pack_holes'] .'</div></div>';
-									echo '<div class="d-flex"><span class="bold">Ciclos</span>:&nbsp;<div class="text">'. $row[0]['pack_cicles'] .'</div></div>';
-
-							echo '</div>
 							</div>
 
 						</section>';
 
-						if($stmt3->num_rows > 0){
+						if($stmt2->num_rows > 0){
 							echo '<section class="related">
 								<div class="title pt-3 pl-3 pr-4">
 									<h2>Máquinas</h2>
 								</div>
 								<div class="slick-carousel-arrows" data-slick=\'{"slidesToShow": 3, "responsive": [{"breakpoint": 831, "settings":{"slidesToShow": 1}}]}\'>';
 
-								for($i = 0; $i < $stmt4->num_rows; $i++){
-									echo '<div class="item"><figure class="image"><a href="./'. $machines_result[$i]['mac_pagename'] .'">';
-									echo '<img src="'. $machines_result[$i]['mac_image'] .'" alt="" width="" height="" />';
-									echo '<figcaption class="title center bold">'. $machines_result[$i]['mac_name'] .'&nbsp;&nbsp;&#10148;</figcaption></a></figure>';
+								for($i = 0; $i < $stmt3->num_rows; $i++){
+									echo '<div class="item"><figure class="image"><a href="'. url() .'/embalagem/'. $packaging_result[$i]['pack_pagename'] .'">';
+									echo '<img src="'. $packaging_result[$i]['pack_image'] .'" alt="" width="" height="" />';
+									echo '<figcaption class="title center bold">'. $packaging_result[$i]['pack_goal'] .'&nbsp;&nbsp;&#10148;</figcaption></a></figure>';
 									echo '</div>';
 								}
 
@@ -122,7 +104,7 @@
 				$type = (isset($_GET['type']) ? $_GET['type'] : 'bobinas');
 
 				$conn->link = $conn->connect();
-				if($stmt = $conn->link->prepare("SELECT * FROM packaging WHERE pack_active = 1")){
+				if($stmt = $conn->link->prepare("SELECT * FROM applications WHERE ap_active = 1")){
 					try{
 						$stmt->execute();
 						$row = get_result($stmt);
@@ -133,16 +115,18 @@
 				}
 				echo '<article class="products">
 					<section class="banner">
-						<figure class="chapas"><figcaption class="right"><h1>Embalagens <br/>para alimentos</h1></figcaption></figure>
+						<figure class="chapas"><figcaption class="right"><h1>Aplicações</h1></figcaption></figure>
 					</section>
 
 					<section class="list">
 						<div class="d-flex">';
 						for($i = 0; $i < $stmt->num_rows; $i++){
 							echo '<figure>
-								<a href="'. url() .'/aplicacao/'. $row[$i]['pack_name'] .'">
-								<img src="'. $row[$i]['pack_image'] .'" alt="" width="" height="" />
-								<figcaption><h3>'. $row[$i]['pack_goal'] .'</h3></figcaption></a>
+								<a href="'. url() .'/aplicacao/'. $row[$i]['ap_name'] .'">
+								<img src="'. $row[$i]['ap_image'] .'" alt="" width="" height="" />
+								<figcaption>
+									<h3>'. $row[$i]['ap_name'] .'</h3>
+								</figcaption></a>
 							</figure>';
 						}
 					echo '</div>
--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
 									foreach($result as $i => $v){
 										echo '<div data-apId="'. $v['ap_id'] .'">
-										<a href="#'. $v['ap_name'] .'">
+										<a href="./aplicacao/'. $v['ap_name'] .'">
 											<img src="'. constant('WEBSITE_ICON_NUMBER_' . $v['ap_icon']) .'" alt="'. $v['ap_name'] .'" width="" height="" />
 										<span>'. $v['ap_name'] .'</span>
 										</a></div>';
